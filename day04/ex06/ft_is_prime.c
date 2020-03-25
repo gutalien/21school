1d6a082ac8d65d7712cbfd0888143d01fceacb52
gutalien <sergey@sevrugin.ru>
2020-03-26 01:02:16 +0300

    is prime

--- day04/ex06/ft_is_prime.c
+++ day04/ex06/ft_is_prime.c
@@ -8,7 +8,7 @@ int		ft_is_prime(int nb)
 	if (nb <= 1 || nb % 2 == 0)
 		return (0);
 	i = 3;
-	while (nb % i && i < 46341)
+	while (nb % i && i * i < nb && i < 46341)
 		i += 2;
 	p = (nb % i) ? 1 : 0;
 	return (p);	
